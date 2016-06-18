49b034e135a384e2db41e471d4a1f104eb8abba6
krisek <kris@lxsystems.de>
2016-06-18 17:44:23 +0200

    Update mediaklikk.php
    cli fix

--- mediaklikk.php
+++ mediaklikk.php
@@ -12,7 +12,7 @@ foreach( $argv as $argument ) {
         $pair = explode( "=", $argument );
         $variableName = substr( $pair[ 0 ], 2 );
         $variableValue = $pair[ 1 ];
-        echo $variableName . " = " . $variableValue . "\n";
+//        echo $variableName . " = " . $variableValue . "\n";
         // Optionally store the variable in $_REQUEST
         $_REQUEST[ $variableName ] = $variableValue;
 }
